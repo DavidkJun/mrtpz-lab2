e6df8b7803dd3eb545f7ea8cb4958defd74b065c
Davyd Krykota <krikotadavid957@gmail.com>
2025-03-26 15:15:18 +0200

    added insert and delete tests

--- circularLinked.test.js
+++ circularLinked.test.js
@@ -16,4 +16,55 @@ describe('CircularLinkedList', () => {
             expect(list.get(0)).toBe('A')
         })
     })
+
+    describe('length', () => {
+        it('should return the length of a list', () => {
+            list.append('A')
+            list.append('B')
+            list.append('C')
+            expect(list.length()).toBe(3)
+        })
+    })
+    describe('Insert', () => {
+        it('should return the list with a new element inserted', () => {
+            list.append('A')
+            list.append('B')
+            list.insert('C',1)
+            expect(list.length()).toBe(3)
+            expect(list.get(1)).toBe('C')
+        });
+    })
+    describe('Insert', () => {
+        it('should throw an Error because of wrong index', () => {
+            expect(() => list.insert('K',-1)).toThrowError('Invalid index')
+        })
+    })
+   describe('Delete', () => {
+       it('should return deleted element', () => {
+           list.append('A')
+           list.append('B')
+           list.append('C')
+           const deletedEl = list.delete(1);
+
+           expect(list.length()).toBe(2)
+           expect(deletedEl).toBe('B')
+       });
+   })
+   describe('Delete', () => {
+       it('should throw an Error because of wrong index', () => {
+           expect(() => list.delete(-1)).toThrowError('Invalid index')
+           expect(() => list.delete(10)).toThrowError('Invalid index')
+       })
+   })
 })
+
+
+
+
+
+
+
+
+
+
+
