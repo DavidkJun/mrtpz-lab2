e89cbae33a4cbdc05b4a2e524f2812d3b053da1a
Davyd Krykota <krikotadavid957@gmail.com>
2025-03-26 21:47:43 +0200

    fix

--- arrayList.test.js
+++ arrayList.test.js
@@ -199,7 +199,7 @@ describe('CircularArrayList', () => {
         it('should extend list with another list', () => {
             const otherList = new CircularArrayList();
             otherList.append('X');
-            otherList.append('Y1');
+            otherList.append('Y');
 
             list.append('A');
             list.extend(otherList);
